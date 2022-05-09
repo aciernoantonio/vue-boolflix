95b3a5e5cb3899ca4a31e057feb4e28d17d1079b
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-09 15:19:21 +0200

    add log request data array on search

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <div id="app">
 
+    <div class="searchbar">
+      <input type="text" v-model="searchText" @keyup="searchedMovie">
+      <button @click="searchedMovie">Search</button>
+    </div>
+
+    <div class="results">
+
+      <div class="movie"></div>
+
+    </div>
+    
+
   </div>
 </template>
 
 <script>
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
     
+  },
+
+  data(){
+    return {
+      movieList: [],
+      link: "https://api.themoviedb.org/3/search/movie?api_key=e089283e85aac817aa741b544b97794c&language=en-US&page=1&include_adult=false",
+      searchText: "",
+    }
+  },
+
+  methods:{
+    searchedMovie(){
+      console.log(this.searchText);
+      axios
+      .get(this.link)
+      .then(response => {
+        this.movie = response.data.results;
+        console.log(this.movie);
+      })
+    }
   }
 }
 </script>
