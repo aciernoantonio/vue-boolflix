57a7cbc1930f5593e338103c2dfb5a110d5140e6
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-10 17:19:07 +0200

    add stars vote script without font awesome

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,16 @@
         <li><h3>{{movie.title}}{{movie.name}}</h3></li>
         <li><h3>{{movie.original_title}}{{movie.original_name}}</h3></li>
         <li><country-flag :country="movie.original_language === 'en' ? 'gb' : movie.original_language" size="normal" /></li>
-        <!-- <li><div>{{roundvote(movie.vote_average)}}</div></li> -->
+        <li>
+          <div v-for="num in roundVote(movie.vote_average)" :key="num">
+            <div>*</div>
+          </div>
+
+          <div v-for="empty in 5 - roundVote(movie.vote_average)" :key="empty">
+            <div>-</div>
+          </div>
+        </li>
+        <!-- per ottenere le stelle vuote poi fai 5 - roundVote e cicli di nuovo -->
 
       </ul>
 
@@ -30,14 +39,14 @@ import axios from "axios"
 export default {
   name: 'App',
   components: {
-    
+
   },
 
   data(){
     return {
       movieList: [],
-      linkMovie: `https://api.themoviedb.org/3/search/movie?api_key=e089283e85aac817aa741b544b97794c&language=en-US&page=1&include_adult=false&query=`,
-      linkTv: `https://api.themoviedb.org/3/search/tv?api_key=e089283e85aac817aa741b544b97794c&language=en-US&page=1&include_adult=false&query=`,
+      linkMovie: `https://api.themoviedb.org/3/search/movie?api_key=e089283e85aac817aa741b544b97794c&language=en-US&page=1&include_adult=true&query=`,
+      linkTv: `https://api.themoviedb.org/3/search/tv?api_key=e089283e85aac817aa741b544b97794c&language=en-US&page=1&include_adult=true&query=`,
       searchText: "",
       reponse: null,
       imgSize: "http://image.tmdb.org/t/p/w500/",
@@ -61,14 +70,15 @@ export default {
       console.log(this.movieList);
       })
 
-    }
+    },
+
+    roundVote(vote){
+   return Math.ceil(vote / 2);
+  }
     
   },
 
-  roundVote(vote){
-    vote = vote / 2;
-    Math.round(vote);
-  }
+  
 
 }
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import CountryFlag from 'vue-country-flag'
+import Vue from 'vue'
+import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 Vue.component('country-flag', CountryFlag)
 
